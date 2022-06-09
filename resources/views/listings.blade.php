c684524821c29936c54666e79a404f7491deae5e
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-06-09 17:08:33 +0700

    build: implement tailwindcss

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Listings</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body class="antialiased">
+    <div class="p-4">
+        <h1 class="text-center text-xl font-semibold">Listings</h1>
+    </div>
+</body>
+
+</html>
